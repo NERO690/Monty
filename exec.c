1f7a78ec57038d495bc26184e1ef578397ead46c
NERO960 <nelsonchristmas35@gmail.com>
2023-10-21 21:15:35 -0700

    updated monty

---
+++ exec.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+/**
+ * executeOpcode - Performs the operation specified by the opcode.
+ * @stack: Pointer to the head of a linked list representing a stack
+ * @line_counter: line_number
+ * @file: Pointer to the Monty file being processed
+ * @line_content: The content of the current line
+ * Return: no return
+ */
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
+{
+	instruction_t opst[] = {
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop", f_pop},
+				{"swap", f_swap},
+				{"add", f_add},
+				{"nop", f_nope},
+				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
+				{"queue", f_queue},
+				{"stack", f_stack},
+				{NULL, NULL}
+				};
+	unsigned int i = 0;
+	char *op;
+
+	op = strtok(content, " \n\t");
+	if (op && op[0] == '#')
+		return (0);
+	bus.arg = strtok(NULL, " \n\t");
+	while (opst[i].opcode && op)
+	{
+		if (strcmp(op, opst[i].opcode) == 0)
+		{	opst[i].f(stack, counter);
+			return (0);
+		}
+		i++;
+	}
+	if (op && opst[i].opcode == NULL)
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+		fclose(file);
+		free(content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE); }
+	return (1);
+}
---
+++ nope.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * noOperation - Does nothing; a placeholder function.
+ * @stack_head: Pointer to the stack's head
+ * @line_number: line_number
+ * Return: no return
+ */
+void f_nope(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
