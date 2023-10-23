5740306ef81d4c4832bbcdde60e5a58ee3f1f952
NERO960 <nelsonchristmas35@gmail.com>
2023-10-22 21:18:27 -0700

    monty 1

--- pall.c
+++ pall.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 /**
- * f_pall - prints stack
+ * f_pall - prints the stack
  * @head: stack head
- * @counter: line_number
- * Return: nothing
+ * @counter: no used
+ * Return: no return
 */
 void f_pall(stack_t **head, unsigned int counter)
 {
